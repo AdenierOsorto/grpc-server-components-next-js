6c5d5bd1170ab6e51cae1347b5034ff35ce975af
adenierosorto <adenierosorto@gmail.com>
2024-03-15 11:30:31 -0600

    added new button for find another client, added a few css in the containter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import type { GetServerSideProps } from 'next';
-
 import { useEffect, useState } from 'react';
 import { getClient } from './components/server';
 interface Client {
@@ -14,21 +12,34 @@ interface Client {
 
 export default function Home() {
   const [client, setClient] = useState<Client>()
+
+  const onClickFindClient = async () => {
+    const { client, error } = await getClient(2);
+    setClient(client as Client)
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       const { client, error } = await getClient(1);
       setClient(client as Client)
-      console.log({error})
     }
     fetchData()
   }, [])
   return (
-    <div className="container">
-      <span>id: {client?.id}</span>
-      <span>FistName: {client?.firstName}</span>
-      <span>LastName: {client?.lastName}</span>
-      <span>Email: {client?.email}</span>
-      <span>Age: {client?.age}</span>
-    </div>
+    <main style={{display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center', width: "100%", height: "100vh", backgroundColor: "#000000"}}>
+      <div style={{display: 'flex', gap:2, flexDirection: "column", backgroundColor: "#eaeaea", padding: "15px 20px", borderRadius: 15, marginBottom: 15, minWidth: "400px"}}>
+        <span><b>id:</b> {client?.id}</span>
+        <br/>
+        <span><b>FistName:</b> {client?.firstName}</span>
+        <br/>
+        <span><b>LastName:</b> {client?.lastName}</span>
+        <br/>
+        <span><b>Email:</b> {client?.email}</span>
+        <br/>
+        <span><b>Age:</b> {client?.age}</span>
+        <br/>
+      </div>
+      <button onClick={onClickFindClient} style={{padding: "5px 25px", backgroundColor: "rgb(14, 165, 233)", color: "white", border: "none", borderRadius: 5}}>find</button>
+    </main>
   );
 }
